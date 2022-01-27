7c97037a95745091383173627d5330aa74867e71
Chris DeVita <cdevita@yahoo.com>
2022-01-27 14:27:45 -0500

    Using files out of repo instead of install location ... not to happy about that

--- check_driver.sh
+++ check_driver.sh
@@ -4,6 +4,6 @@
 # 
 if [ `ps -ef | grep hw_device2.py| grep -v grep| wc -l| grep 0` ] ; then 
    logger "Restarting hotwater heater device driver V2( hw_device2.py)"; 
-   /usr/bin/python /home/pi/hw_heater/hw_device2.py & 
+   /usr/bin/python3 /home/pi/hw_heater/repo/py/hw_device2.py & 
 fi
 
