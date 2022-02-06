931d4fa7d548ccbc350f49ff08184946b4bba3c1
Chris DeVita <cdevita@yahoo.com>
2022-02-06 18:11:33 -0500

    Calling mosquitto_pub in cron script to turnon/off the hotwater heater

---
+++ sh/turnoff.sh
@@ -0,0 +1,2 @@
+ mosquitto_pub -h homeassistant.hm -t hotwater-v2/power -m OFF -u mqttuser -P mqttpass -i cron.$$
+
---
+++ sh/turnon.sh
@@ -0,0 +1,2 @@
+ mosquitto_pub -h homeassistant.hm -t hotwater-v2/power -m ON -u mqttuser -P mqttpass -i cron.$$
+
