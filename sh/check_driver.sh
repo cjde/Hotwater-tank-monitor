20811c9dbf9f56436ee85d15dab879b06152b6a8
Chris DeVita <cjdevita@yahoo.com>
2022-02-09 11:28:54 -0500

    Adjusting for virtual env

--- sh/check_driver.sh
+++ sh/check_driver.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 # When the router rebbots on Sundays the network drops and we loos connectivity to the 
 # the MQTT server.. so we just restart 
-# 
+#
+venvpath="/home/pi/hw-venv"
 if [ `ps -ef | grep hw_device2.py| grep -v grep| wc -l| grep 0` ] ; then 
-   logger "Restarting hotwater heater device driver V2( hw_device2.py)"; 
-   /usr/bin/python3 /home/pi/hw_heater/repo/py/hw_device2.py & 
+   logger "Restarting hotwater heater device driver V2( hw_device2.py)";
+   source ${venvpath}/bin/activate
+   python3 ${venvpath}Hotwater-tank-monitor/py/hw_device2.py &
 fi
 
--- sh/turnoff.sh
+++ sh/turnoff.sh
@@ -1,2 +1,2 @@
- mosquitto_pub -h homeassistant.hm -t hotwater-v2/power -m OFF -u mqttuser -P mqttpass -i cron.$$
+mosquitto_pub -h homeassistant.hm -t hotwater-v2/power -m OFF -u mqttuser -P mqttpass -i cron.$$
 
