e026f55d37a1732982ce50a03c27ec91a5a2315e
Chris DeVita <cdevita@yahoo.com>
2022-02-09 14:34:01 -0500

    wrapper scripts for starting in a virtual env

--- sh/check_driver.sh
+++ sh/check_driver.sh
@@ -2,10 +2,11 @@
 # When the router rebbots on Sundays the network drops and we loos connectivity to the 
 # the MQTT server.. so we just restart 
 #
+script="hw_device2.py"
 venvpath="/home/pi/hw-venv"
-if [ `ps -ef | grep hw_device2.py| grep -v grep| wc -l| grep 0` ] ; then 
-   logger "Restarting hotwater heater device driver V2( hw_device2.py)";
+if [ `ps -ef | grep $script| grep -v grep| wc -l| grep 0` ] ; then 
+   logger "Restarting hotwater power monitor( ${script} )";
    source ${venvpath}/bin/activate
-   python3 ${venvpath}Hotwater-tank-monitor/py/hw_device2.py &
+   python3 ${venvpath}/Hotwater-tank-monitor/py/${script} >> /tmp/hotwater.log &
 fi
 
---
+++ sh/collect_temps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+# When the router rebbots on Sundays the network drops and we loose connectivity to the 
+# the MQTT server.. so we just restart 
+#
+script="HW_temps2.py"
+venvpath="/home/pi/hw-venv"
+if [ `ps -ef | grep $script| grep -v grep| wc -l| grep 0` ] ; then 
+   logger "Collecting hotwater temperature ( ${script} )";
+   source ${venvpath}/bin/activate
+   python3 ${venvpath}/Hotwater-tank-monitor/py/${script} -m -v -d >> /tmp/hotwater.log 
+fi
+
