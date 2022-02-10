2dfdac4e9f4714c4cb06ffca68c680187bb1b55e
Chris DeVita <cdevita@yahoo.com>
2022-02-10 17:11:18 -0500

    hw_temps2.py script name changed. Changing it here too

--- sh/collect_temps.sh
+++ sh/collect_temps.sh
@@ -2,12 +2,12 @@
 # When the router rebbots on Sundays the network drops and we loose connectivity to the 
 # the MQTT server.. so we just restart 
 #
-script="HW_temps2.py"
+script="hw_temps2.py"
 venvpath="/home/pi/hw-venv"
 cd  ${venvpath}/Hotwater-tank-monitor 
 if [ `ps -ef | grep $script| grep -v grep| wc -l| grep 0` ] ; then 
    logger "Collecting hotwater temperature ( ${script} )";
    source ${venvpath}/bin/activate
-   python3 ${venvpath}/Hotwater-tank-monitor/py/${script} -m -v -d >> /tmp/hotwater.log 
+   python3 ${venvpath}/Hotwater-tank-monitor/py/${script} -m -v >> /tmp/hotwater.log 
 fi
 
